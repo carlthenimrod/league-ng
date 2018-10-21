05112c7bbfa9f1bb18e3cef621f0cde2d254d203
Carl Dawson <carl@invisible-gorilla.com>
2018-10-21 07:39:20 -0400

    Added HomeComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './core/home/home.component';
 
 const routes: Routes = [
-  { path: 'admin', loadChildren: './admin/admin.module#AdminModule'}
+  { path: '', component: HomeComponent },
+  { path: 'admin', loadChildren: './admin/admin.module#AdminModule' }
 ];
 
 @NgModule({
