c0da1d370b151e40a91b51cd75b2cf4110872e7c
Carl Dawson <carl@invisible-gorilla.com>
2018-11-16 08:16:08 -0500

    Added animations, added animations for unassigned teams

--- src/app/admin/admin-leagues/admin-league/admin-league-overview/admin-league-overview.component.ts
+++ src/app/admin/admin-leagues/admin-league/admin-league-overview/admin-league-overview.component.ts
@@ -7,11 +7,13 @@ import { AdminLeagueTeamModalComponent } from './admin-league-team-modal/admin-l
 import { League, Division } from '@app/models/league';
 import { LeagueService } from '@app/core/league.service';
 import { Team } from '@app/models/team';
+import { unassignedTeamsToggleTrigger, unassignedTeamEnterTrigger } from './animations';
 
 @Component({
   selector: 'app-admin-league-overview',
   templateUrl: './admin-league-overview.component.html',
-  styleUrls: ['./admin-league-overview.component.scss']
+  styleUrls: ['./admin-league-overview.component.scss'],
+  animations: [unassignedTeamsToggleTrigger, unassignedTeamEnterTrigger]
 })
 export class AdminLeagueOverviewComponent implements OnInit, OnDestroy {
 
---
+++ src/app/admin/admin-leagues/admin-league/admin-league-overview/animations.ts
@@ -0,0 +1,45 @@
+import { trigger, transition, style, animate, query, stagger } from '@angular/animations';
+
+export const unassignedTeamsToggleTrigger = trigger('unassignedTeamsToggle', [
+  transition(':enter', [
+    query('button', [
+      style({
+        transform: 'scale(0)'
+      })
+    ], { optional: true }),
+    style({
+      height: 0,
+      opacity: 0
+    }),
+    animate(100, style({
+      height: '*'
+    })),
+    animate(100),
+    query('button', [
+      stagger(100, [
+        animate(100, style({
+          transform: 'scale(1)'
+        }))
+      ])
+    ], { optional: true })
+  ]),
+  transition(':leave', [
+    animate(100, style({
+      opacity: 0
+    })),
+    animate(100, style({
+      height: 0
+    }))
+  ])
+]);
+
+export const unassignedTeamEnterTrigger = trigger('unassignedTeamEnter', [
+  transition(':enter', [
+    style({
+      transform: 'scale(0)'
+    }),
+    animate(200, style({
+      transform: 'scale(1)'
+    }))
+  ])
+]);
