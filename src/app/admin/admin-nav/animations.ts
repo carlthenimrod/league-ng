989a9d064d73e9b9959317125a52d915731400c7
Carl Dawson <carl@invisible-gorilla.com>
2018-11-21 13:19:03 -0500

    Animation tweak

--- src/app/admin/admin-nav/animations.ts
+++ src/app/admin/admin-nav/animations.ts
@@ -5,6 +5,7 @@ export const slideNavTrigger = trigger('slideNav', [
     transform: 'translateX(0%)'
   })),
   state('closed', style({
+    opacity: 0,
     transform: 'translateX(100%)'
   })),
   transition('closed => open', [
@@ -13,12 +14,14 @@ export const slideNavTrigger = trigger('slideNav', [
     ]),
     animate(100),
     query('a', [
-      stagger(100, [
+      stagger(50, [
         animate(100, style({ opacity: 1 }))
       ])
     ])
   ]),
   transition('open => closed', [
-    animate(200)
+    animate(50, style({
+      opacity: 0
+    }))
   ])
 ]);
