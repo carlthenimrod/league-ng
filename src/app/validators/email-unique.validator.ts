ff275a434ec9bb0dc9d704abef56c16b01340c7c
Carl Dawson <carl@invisible-gorilla.com>
2019-01-11 05:40:30 -0500

    Re-arranged imports for email validator

--- src/app/validators/email-unique.validator.ts
+++ src/app/validators/email-unique.validator.ts
@@ -1,15 +1,15 @@
 import { AbstractControl, AsyncValidatorFn, ValidationErrors } from '@angular/forms';
 import { Observable, timer, of } from 'rxjs';
+import { switchMap, mapTo, catchError } from 'rxjs/operators';
 
 import { UserService } from '@app/core/user.service';
-import { switchMap, mapTo, catchError } from 'rxjs/operators';
 
 export const emailUnique = (userService: UserService): AsyncValidatorFn => {
   return (control: AbstractControl): Observable<ValidationErrors | null> => {
     return timer(500).pipe(switchMap(() => {
       return userService.checkEmail(control.value).pipe(
         mapTo(null),
-        catchError(err => of({notUnique: true}))
+        catchError(() => of({notUnique: true}))
       );
     }));
   };
