f3f80fdbdfa3f71b1adf4f767daa6fa4286b0e5d
Carl Dawson <carl@invisible-gorilla.com>
2019-01-11 05:32:12 -0500

    Updated unique email validator to prevent flickering of form validity

--- src/app/validators/email-unique.validator.ts
+++ src/app/validators/email-unique.validator.ts
@@ -1,20 +1,16 @@
 import { AbstractControl, AsyncValidatorFn, ValidationErrors } from '@angular/forms';
-import { Observable, Observer } from 'rxjs';
+import { Observable, Observer, timer, of } from 'rxjs';
 
 import { UserService } from '@app/core/user.service';
+import { switchMap, mapTo, catchError } from 'rxjs/operators';
 
 export const emailUnique = (userService: UserService): AsyncValidatorFn => {
   return (control: AbstractControl): Observable<ValidationErrors | null> => {
-    return Observable.create((obs: Observer<ValidationErrors | null>) => {
-      if (control.value) {
-        userService.checkEmail(control.value).subscribe(() => {
-          obs.next(null);
-          obs.complete();
-        }, () => {
-          obs.next({ notUnique: true });
-          obs.complete();
-        });
-      }
-    });
+    return timer(500).pipe(switchMap(() => {
+      return userService.checkEmail(control.value).pipe(
+        mapTo(null),
+        catchError(err => of({notUnique: true}))
+      );
+    }));
   };
 };
