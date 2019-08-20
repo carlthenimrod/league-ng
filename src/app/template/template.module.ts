3c4fe9e133ad630c8616e862918fc74b8122a3eb
Carl Dawson <carl@invisible-gorilla.com>
2019-08-19 23:16:15 -0400

    Added search to top navigation - front end only

--- src/app/template/template.module.ts
+++ src/app/template/template.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { TemplateComponent } from './template.component';
 import { NavComponent } from './header/nav/nav.component';
 import { UserNotificationsComponent } from './header/user-notifications/user-notifications.component';
+import { SearchComponent } from './header/nav/search/search.component';
 
 @NgModule({
   declarations: [
@@ -14,11 +16,13 @@ import { UserNotificationsComponent } from './header/user-notifications/user-not
     FooterComponent, 
     TemplateComponent, 
     NavComponent,
-    UserNotificationsComponent
+    UserNotificationsComponent,
+    SearchComponent
   ],
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    ReactiveFormsModule
   ],
   exports: [
     TemplateComponent
