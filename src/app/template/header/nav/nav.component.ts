62634ed081212263a82b8157f10be68232bc2083
Carl Dawson <carl@invisible-gorilla.com>
2019-08-20 07:50:47 -0400

    Added tabs to mobile nav

--- src/app/template/header/nav/nav.component.ts
+++ src/app/template/header/nav/nav.component.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav.component.scss']
 })
 export class NavComponent implements OnInit {
+  selected: string = 'home';
+
   constructor() { }
 
   ngOnInit() {
