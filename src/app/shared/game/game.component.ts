67be9286023538e079b66c9a87ca384109f8cc28
Carl Dawson <carl@invisible-gorilla.com>
2019-09-20 23:37:29 -0400

    Check logged in user - game component

--- src/app/shared/game/game.component.ts
+++ src/app/shared/game/game.component.ts
@@ -1,23 +1,66 @@
-import { Component, OnInit, Input, HostBinding } from '@angular/core';
+import { Component, OnInit, Input, HostBinding, OnDestroy, OnChanges } from '@angular/core';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 import { Game } from '@app/models/game';
+import { AuthService } from '@app/auth/auth.service';
+import { Auth } from '@app/models/auth';
 
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
   styleUrls: ['./game.component.scss']
 })
-export class GameComponent implements OnInit {
+export class GameComponent implements OnInit, OnChanges, OnDestroy {
   @Input() game: Game;
   @Input() size: string;
   @Input() class = '';
-  @HostBinding('class') get classes(): string {
-    return [this.class, this.size].join(' ');
+  @HostBinding('class.sm') get addClass(): boolean {
+    return (this.size === 'sm') ? true : false;
   }
+  auth: Auth;
+  loggedIn: boolean;
+  isHome: boolean;
+  isAway: boolean;
+  unsubscribe$ = new Subject<void>();
 
-  constructor() { }
+  constructor(
+    private authService: AuthService
+  ) { }
 
   ngOnInit() {
+    this.authService.loggedIn$()
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe(loggedIn => {
+        this.loggedIn = loggedIn;
+        this.auth = this.authService.getAuth();
+        this.checkAwayOrHome();
+      });
   }
 
+  ngOnChanges() {
+    this.checkAwayOrHome();
+  }
+
+  isUser(homeOrAway: string): boolean {
+    if (homeOrAway === 'home' && this.isHome) { return true; }
+    if (homeOrAway === 'away' && this.isAway) { return true; }
+    return false;
+  }
+
+  checkAwayOrHome() {
+    if (this.loggedIn) {
+      const teamIds = this.auth.teams.map(team => team._id);
+      this.isHome = (teamIds.includes(this.game.home._id)) ? true : false;
+      this.isAway = (teamIds.includes(this.game.away._id)) ? true : false;
+    } else {
+      this.isHome = false;
+      this.isAway = false;
+    }
+  }
+
+  ngOnDestroy() {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
+  }
 }
